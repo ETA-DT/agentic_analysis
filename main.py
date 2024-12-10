7d00409dcab55b338e2bfbffa2ee59839f63d369
ETA-DT <165398598+ETA-DT@users.noreply.github.com>
2024-12-10 15:02:43 +0100

    includes dataframe in expander

--- main.py
+++ main.py
@@ -358,7 +358,10 @@ def run_crewai_app():
         )
 
     current_dataframe = view_dataframe(cube_name, view_name)
-    st.dataframe(current_dataframe, hide_index=True, width=800)
+    
+    with st.expander("See Data")
+        st.dataframe(current_dataframe, hide_index=True, width=800)
+    
     def dataframe_prompt_input(cube_name, view_name):
         dataframe = view_dataframe(cube_name, view_name)
         dataframe_md = dataframe.to_markdown()
