f88d5383c621741026d47f14cd526dd7dfee8c95
ETA-DT <165398598+ETA-DT@users.noreply.github.com>
2024-12-17 10:57:37 +0100

    widens dataframe

--- main.py
+++ main.py
@@ -502,7 +502,7 @@ def run_crewai_app():
     current_dataframe = view_dataframe(cube_name, view_name)
     
     with st.expander("See Data"):
-        st.dataframe(current_dataframe, hide_index=True, width=800)
+        st.dataframe(current_dataframe, hide_index=True)
     
     def dataframe_prompt_input(cube_name, view_name):
         dataframe = view_dataframe(cube_name, view_name)
